16663ab73f4b518134b663ad8814978f9cadd452
Arya <38251756+AryaGITOfficial@users.noreply.github.com>
2020-12-14 03:04:55 +0530

    Add files via upload

---
+++ storage/framework/views/5f3d38e861756a0d813d3e87371b55d7212fc52b.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Import CSV Data to MySQL database with Laravel</title>
+  </head>
+  <body>
+     <!-- Message -->
+     <?php if(Session::has('message')): ?>
+        <p ><?php echo e(Session::get('message')); ?></p>
+     <?php endif; ?>
+
+     <!-- Form -->
+     <form method='post' action='/uploadFile' enctype='multipart/form-data' >
+       <?php echo e(csrf_field()); ?>
+
+       <input type='file' name='file' >
+       <input type='submit' name='submit' value='Import'>
+     </form>
+  </body>
+</html><?php /**PATH /var/www/html/Project_CSV_Upload/resources/views/import.blade.php ENDPATH**/ ?>
