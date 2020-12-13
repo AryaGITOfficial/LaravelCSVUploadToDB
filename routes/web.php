277c1915f1defdc83fdfb94a4a5a70d55a54cf58
Arya <38251756+AryaGITOfficial@users.noreply.github.com>
2020-12-14 03:04:09 +0530

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', 'CSVFilesController@index'); // localhost:8000/
+//Route::post('/uploadFile', 'CSVFilesController@uploadFile');
+
+Route::get('/', 'CSVFilesController@create');
+Route::post('/uploadFile', 'CSVFilesController@store');
