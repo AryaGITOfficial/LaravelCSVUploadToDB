259aa4f1103d36a33f49e8e504d875b0a2a3c313
Arya <38251756+AryaGITOfficial@users.noreply.github.com>
2020-12-14 15:53:09 +0530

    Update CSVFilesController.php

--- app/Http/Controllers/CSVFilesController.php
+++ app/Http/Controllers/CSVFilesController.php
@@ -17,21 +17,74 @@ class CSVFilesController extends Controller{
             'file' => 'required|mimes:csv,txt'
         ]);
 
-        $file = file($request->file->getRealPath());
-
-        $headerRow = $file->first()->keys()->toArray();
+        $headerRow = $request->file->first()->keys()->toArray();
         $validate = $this->validateHeaderRow($headerRow);
 
+        $file = file($request->file->getRealPath());
         $data = array_slice($file, 1);
-        $parts = (array_chunk($data, 1000));
+        if( $validate == true ){
+            $parts = (array_chunk($data, 1000));
 
-        foreach($parts as $index=>$part){
-            $filename = resource_path('pending-files/'.date('y-m-d-H-i-s').$index. '.csv');
-            
-            file_put_contents($filename, $part);            
+            foreach($parts as $index=>$part){
+                $filename = resource_path('pending-files/'.date('y-m-d-H-i-s').$index. '.csv');
+                
+                file_put_contents($filename, $part);            
+            }
         }
+            (new AppCsv_files())->importToDB();
+        session()->flash('status','queued for importing.');
+        return redirect('/');
+        
+    }
 
-        (new AppCsv_files())->importToDB();
+    public function validateHeaderRow($headerRow)
+    {
+        $validate = false;
+
+        if( $headerRow[0] == 'Module_code' && $headerRow[1] == 'Module_name' && $headerRow[2] == 'Module_term' ){
+                $validate = true;
+        } 
+
+        return $validate;
+
+    }
+
+  
+}
+<?php
+
+namespace App\Http\Controllers;
+
+use App\csv_files;
+use App\Csv_files as AppCsv_files;
+use Session;
+use Illuminate\Http\Request;
+
+class CSVFilesController extends Controller{
+    public function create(){
+        return view('import');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'file' => 'required|mimes:csv,txt'
+        ]);
+
+        $headerRow = $request->file->first()->keys()->toArray();
+        $validate = $this->validateHeaderRow($headerRow);
+
+        $file = file($request->file->getRealPath());
+        $data = array_slice($file, 1);
+        if( $validate == true ){
+            $parts = (array_chunk($data, 1000));
+
+            foreach($parts as $index=>$part){
+                $filename = resource_path('pending-files/'.date('y-m-d-H-i-s').$index. '.csv');
+                
+                file_put_contents($filename, $part);            
+            }
+        }
+            (new AppCsv_files())->importToDB();
         session()->flash('status','queued for importing.');
         return redirect('/');
         
